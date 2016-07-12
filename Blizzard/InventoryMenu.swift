6840812e3ea12c8316518db9d970f78c16227c1e
dentonhui <dentonhui@msn.com>
2016-07-12 16:15:41 -0700

    Finished adding empty menu classes.

---
+++ Blizzard/InventoryMenu.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryMenu.swift
+//  Blizzard
+//
+//  Created by Denton Hui on 7/12/16.
+//  Copyright © 2016 Denton Hui. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class InventoryMenu: SKScene {
+    
+    // UI Connections
+    
+    // Iterate through character's inventory array and display
+}
---
+++ Blizzard/TownMenu.swift
@@ -0,0 +1,34 @@
+//
+//  TownMenu.swift
+//  Blizzard
+//
+//  Created by Denton Hui on 7/12/16.
+//  Copyright © 2016 Denton Hui. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class TownMenu: SKScene {
+    
+    // UI Connections
+    var buttonQuest: MSButtonNode!
+    var buttonStore: MSButtonNode!
+    
+    override func didMoveToView(view: SKView) {
+        
+        // Set UI Connections
+        buttonQuest = self.childNodeWithName("buttonQuit") as! MSButtonNode
+        buttonStore = self.childNodeWithName("buttonStore") as! MSButtonNode
+        
+        // Set save button handler
+        buttonQuest.selectedHandler = {
+            
+            // Insert quest menu
+        }
+        buttonStore.selectedHandler = {
+            
+            // Insert store menu
+        }
+    }
+}
