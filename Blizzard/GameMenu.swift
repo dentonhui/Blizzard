819d6ee1a397426dc4ce99a7b1322dec44272e34
dentonhui <dentonhui@msn.com>
2016-07-12 16:06:48 -0700

    Added empty overlay menus and main menu.

---
+++ Blizzard/GameMenu.swift
@@ -0,0 +1,28 @@
+//
+//  GameMenu.swift
+//  Blizzard
+//
+//  Created by Denton Hui on 7/12/16.
+//  Copyright © 2016 Denton Hui. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GameMenu: SKScene {
+    
+    // UI Connections
+    var buttonSave: MSButtonNode!
+    
+    override func didMoveToView(view: SKView) {
+        
+        // Set UI Connections
+        buttonSave = self.childNodeWithName("buttonQuit") as! MSButtonNode
+        
+        // Set save button handler
+        buttonSave.selectedHandler = {
+         
+            //Insert save function
+        }
+    }
+}
